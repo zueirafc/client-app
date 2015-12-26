4dc49b97642c7cfdc058c90c312f7e138674d490
Bruno Casali <brunoocasali@gmail.com>
2015-12-26 00:26:29 -0200

    removed many items

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// scripts
+app.use('/scripts', express.static(__dirname + '/public/js'));
+app.use('/vendor', express.static(__dirname + '/node_modules'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
