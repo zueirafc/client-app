0314c2e8db983f8745e05cf115d4354a8a73dec5
Bruno Casali <brunoocasali@gmail.com>
2015-12-26 00:50:45 -0200

    removed unused files, added new server.js (working now) and #gogogo

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ var app = express();
 var path = require('path');
 
 // scripts
-app.use('/scripts', express.static(__dirname + '/resources/bower'));
-app.use('/styles', express.static(__dirname + '/resources/css'));
-app.use('/vendor', express.static(__dirname + '/node_modules'));
+app.use('/scripts', express.static(__dirname + '/app/resources/bower'));
+app.use('/scripts/core', express.static(__dirname + '/app/resources/core'));
+app.use('/components', express.static(__dirname + '/app/resources/components'));
+app.use('/styles', express.static(__dirname + '/app/resources/css'));
+app.use('/vendor', express.static(__dirname + '/app/node_modules'));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + 'app/index.html'));
+    res.sendFile(path.join(__dirname + '/app/index.html'));
 });
 
 app.set('port', (process.env.PORT || 5000));
