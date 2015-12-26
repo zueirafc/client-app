45bfe5b0fafea4033d8da36463a6bb2a3cbc68f2
Bruno Casali <brunoocasali@gmail.com>
2015-12-26 13:56:38 -0200

    new background image new views, removed unused files

--- app/resources/core/app.js
+++ app/resources/core/app.js
@@ -1,14 +1,28 @@
 'use strict';
 
 // Defining Angular app model with all other dependent modules
-var App = angular.module('App',['ngRoute',
-	'App.home','App.about','App.login']);
+var App = angular.module('App', ['ngRoute']);
+
+App.config(function ($routeProvider) {
+    $routeProvider.when('/', {
+        controller: 'HomeController'
+    });
+
+    $routeProvider.when('/login', {
+        templateUrl: 'components/views/login.html',
+        controller: 'LoginController'
+    });
+
+    $routeProvider.otherwise({
+        templateUrl: 'components/views/404.html'
+    });
+});
 
 App.config(function($routeProvider, $locationProvider, $httpProvider) {
 
 	// Declaration of the default route if neither of the controllers
 	// is supporting the request path
-	$routeProvider.otherwise({ redirectTo: '/'});
+	// $routeProvider.otherwise({ redirectTo: '/'});
 
 	// Settings for http communications
 	$httpProvider.defaults.useXDomain = true;
