6a8acf3fa8ed5bbffa9edebc3088cd4977679ad1
Bruno Casali <brunoocasali@gmail.com>
2016-02-29 11:51:04 -0300

    updated home page, fix angular error, and added new toolbar version

--- app/resources/components/controllers/ToolbarController.js
+++ app/resources/components/controllers/ToolbarController.js
@@ -1,3 +1,11 @@
-App.controller('ToolbarController', function($scope, $location) {
-  $scope.templateUrl = 'components/partials/toolbar/_admin.html';
+App.controller('ToolbarController', function($scope, $location, $http, Api) {
+  $scope.templateUrl = 'components/partials/toolbar/_logged.html';
+
+  $scope.getClubs = function(){
+    $http.get(Api + '/clubs.json').then(function successCallback(response) {
+        $scope.clubs = response.data.clubs;
+    });
+  };
+
+  $scope.getClubs();
 });
