b0bbd21528f1b069281409ff102852f38996a9c9
cleitonMigliorinii <cleitondev@gmail.com>
2016-03-11 17:15:07 -0300

    Ajuste
    Configuração Mineração de Post

--- app/resources/components/controllers/admin/ApprovalsController.js
+++ app/resources/components/controllers/admin/ApprovalsController.js
@@ -1,40 +1,109 @@
-App.factory('ZueiraAPI', function($http, Micropost, Api) {
+App.factory('ZueiraAPI', function($http, Micropost, Api,$log) {
   var ZueiraAPI = function() {
     this.items = [];
     this.busy = false;
     this.nextPageNumber = 1;
   };
 
-  ZueiraAPI.prototype.nextPage = function() {
+  ZueiraAPI.prototype.nextPage = function(typePost) {
     if (this.busy) return;
     this.busy = true;
 
-    Micropost.pending({page: this.nextPageNumber}, function(data){
-      var items = data.microposts;
 
-      for (var i = 0; i < items.length; i++) {
-        this.items.push(items[i]);
-      }
+	if(typePost == 0){
 
-      this.nextPageNumber += 1;
-      this.busy = false;
-    }.bind(this));
-  };
+	    Micropost.deleted({page: this.nextPageNumber}, function(data){
+	      items = data.microposts;
+	      for (var i = 0; i < items.length; i++) {
+	        this.items.push(items[i]);
+	      }
+
+	      this.nextPageNumber += 1;
+	      this.busy = false;
+	    }.bind(this));
+	};
+
+
+	if(typePost == 1){
+
+	    Micropost.banned({page: this.nextPageNumber}, function(data){
+	      items = data.microposts;
+	      for (var i = 0; i < items.length; i++) {
+	        this.items.push(items[i]);
+	      }
+
+	      this.nextPageNumber += 1;
+	      this.busy = false;
+	    }.bind(this));
+	};
+
+	if(typePost == 2){
+
+	    Micropost.active({page: this.nextPageNumber}, function(data){
+	      items = data.microposts;
+	      for (var i = 0; i < items.length; i++) {
+	        this.items.push(items[i]);
+	      }
+
+	      this.nextPageNumber += 1;
+	      this.busy = false;
+	    }.bind(this));
+	};
+
+
+	if(typePost == 3){
+
+	    Micropost.reproved({page: this.nextPageNumber}, function(data){
+	      items = data.microposts;
+	      for (var i = 0; i < items.length; i++) {
+	        this.items.push(items[i]);
+	      }
+
+	      this.nextPageNumber += 1;
+	      this.busy = false;
+	    }.bind(this));
+	};
+
+	if(typePost == 4){
+	    Micropost.pending({page: this.nextPageNumber}, function(data){
+	      var items = data.microposts;
+
+
+	      for (var i = 0; i < items.length; i++) {
+	        this.items.push(items[i]);
+	      }
+
+	      this.nextPageNumber += 1;
+	      this.busy = false;
+	    }.bind(this));
+	};
+
+
+	};
 
   return ZueiraAPI;
 });
 
 
-App.controller('ApprovalsController', function($scope, Micropost, ZueiraAPI, MicropostParticipant, $http) {
+App.controller('ApprovalsController', function($scope, Micropost, ZueiraAPI, MicropostParticipant, $http,$log) {
 	// deleted: 0, banned: 1, active: 2, reproved: 3, pending: 4
 
 	$scope.api = new ZueiraAPI();
+	$scope.typePost = 4;
 
 	$scope.open = function (post) {
 		$scope.post = post;
 
 		$('.ui.modal').modal('show');
 	};
+
+	$scope.mudarTypePost = function (typePost) {
+		$scope.typePost = typePost;
+		$scope.api.items = [];
+		$scope.api.nextPageNumber = 1;
+		$scope.api.nextPage($scope.typePost);
+	};
+
 
 	$scope.init = function(){
    		MicropostParticipant.query(function(data){
@@ -51,6 +120,12 @@ App.controller('ApprovalsController', function($scope, Micropost, ZueiraAPI, Mic
 	$scope.approve = function(){
 		$scope.post.status = 2;
 
+		$scope.micropostJson = {
+    		"micropost" :$scope.post
+    	};
+    	
+		Micropost.update({ id:$scope.post.id }, $scope.micropostJson);
+
 		$('.ui.modal').modal('hide');
 
 	};
@@ -58,17 +133,28 @@ App.controller('ApprovalsController', function($scope, Micropost, ZueiraAPI, Mic
 	$scope.reprove = function(){
 		$scope.post.status = 3;
 
+		$scope.micropostJson = {
+    		"micropost" :$scope.post
+    	};
 
-			$('.ui.modal').modal('hide');
+		Micropost.update({ id:$scope.post.id }, $scope.micropostJson);
+
+		$('.ui.modal').modal('hide');
 	};
 
 	$scope.remove = function(){
 		$scope.post.status = 1;
 
+		$scope.micropostJson = {
+    		"micropost" :$scope.post
+    	};
+    	
+		Micropost.update({ id: $scope.post.id}, $scope.micropostJson);
+
 		$('.ui.modal').modal('hide');
 	
 	};
-	
+
 
 	$scope.init();
 });
--- app/resources/components/resources/micropost.js
+++ app/resources/components/resources/micropost.js
@@ -1,13 +1,47 @@
 App.factory('Micropost', function($resource, Api) {
-	return $resource(Api + '/microposts/:id.json', { id: '@id' },
+
+    var resource = $resource(Api + '/microposts/:id.json', { id: '@id' },
 	{
 		update: { method: 'PUT' },
 		query: { method: 'GET', isArray: false },
+        deleted: { 
+            url: Api + '/microposts/deleted', 
+            method: 'GET', 
+            isArray: false
+        },
+        banned: { 
+            url: Api + '/microposts/banned', 
+            method: 'GET', 
+            isArray: false
+        },
+        active: { 
+            url: Api + '/microposts/active', 
+            method: 'GET', 
+            isArray: false
+        },
+        reproved: { 
+            url: Api + '/microposts/reproved', 
+            method: 'GET', 
+            isArray: false
+        },
         pending: { 
             url: Api + '/microposts/pending', 
             method: 'GET', 
             isArray: false
-        },
-        active: { url: Api + '/microposts/active', method: 'GET', isArray: false }
+        }
 	});
+
+    return resource;
+
 });
+
+/*
+
+  # deleted: Quando eu vejo uma zueira que nao eh uma zueira na realidade
+  # banned: Quando deu treta e alguem quer meu coro porque usei uma imagem prop.
+  # active: As zueiras que serao exibidas lah
+  # reproved: Zueiras que por algum motivo nao especificado sao reprovadas.
+  # pending: Zueiras recentes que acabam de passar pelo crawler.
+  associate_values deleted: 0, banned: 1, active: 2, reproved: 3, pending: 4
+
+*/
