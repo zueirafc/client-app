559607cb5e42fd64d64d16633aba2f2b6e07d4a7
cleitonMigliorinii <cleitondev@gmail.com>
2016-04-28 21:37:25 -0300

    test select and dropdown

--- app/resources/components/controllers/admin/ApprovalsController.js
+++ app/resources/components/controllers/admin/ApprovalsController.js
@@ -1,87 +1,87 @@
 App.factory('ZueiraAPI', function($http, Micropost, Api,$log) {
-  
-  var ZueiraAPI = function() {
-    this.items = new Array();
-    this.busy = false;
-    this.nextPageNumber = 1;
-  };
-
-  ZueiraAPI.prototype.nextPage = function(typePost) {
-    if (this.busy) return;
-    this.busy = true;
-
-	if(typePost == 0){
-
-	    Micropost.deleted({page: this.nextPageNumber}, function(data){
-	      items = data.microposts;
-	      for (var i = 0; i < items.length; i++) {
-	        this.items.push(items[i]);
-	      }
-
-	      this.nextPageNumber += 1;
-	      this.busy = false;
-	    }.bind(this));
+	
+	var ZueiraAPI = function() {
+		this.items = new Array();
+		this.busy = false;
+		this.nextPageNumber = 1;
 	};
 
+	ZueiraAPI.prototype.nextPage = function(typePost) {
+		if (this.busy) return;
+		this.busy = true;
 
-	if(typePost == 1){
+		if(typePost == 0){
 
-	    Micropost.banned({page: this.nextPageNumber}, function(data){
-	      items = data.microposts;
-	      for (var i = 0; i < items.length; i++) {
-	        this.items.push(items[i]);
-	      }
+			Micropost.deleted({page: this.nextPageNumber}, function(data){
+				items = data.microposts;
+				for (var i = 0; i < items.length; i++) {
+					this.items.push(items[i]);
+				}
 
-	      this.nextPageNumber += 1;
-	      this.busy = false;
-	    }.bind(this));
+				this.nextPageNumber += 1;
+				this.busy = false;
+			}.bind(this));
+		};
+
+
+		if(typePost == 1){
+
+			Micropost.banned({page: this.nextPageNumber}, function(data){
+				items = data.microposts;
+				for (var i = 0; i < items.length; i++) {
+					this.items.push(items[i]);
+				}
+
+				this.nextPageNumber += 1;
+				this.busy = false;
+			}.bind(this));
+		};
+
+		if(typePost == 2){
+
+			Micropost.active({page: this.nextPageNumber}, function(data){
+				items = data.microposts;
+				for (var i = 0; i < items.length; i++) {
+					this.items.push(items[i]);
+				}
+
+				this.nextPageNumber += 1;
+				this.busy = false;
+			}.bind(this));
+		};
+
+
+		if(typePost == 3){
+
+			Micropost.reproved({page: this.nextPageNumber}, function(data){
+				items = data.microposts;
+				for (var i = 0; i < items.length; i++) {
+					this.items.push(items[i]);
+				}
+
+				this.nextPageNumber += 1;
+				this.busy = false;
+			}.bind(this));
+		};
+
+		if(typePost == 4){
+			Micropost.pending({page: this.nextPageNumber}, function(data){
+				var items = data.microposts;
+
+				
+				for (var i = 0; i < items.length; i++) {
+					this.items.push(items[i]);
+				}
+
+				this.nextPageNumber += 1;
+				this.busy = false;
+			}.bind(this));
+		};
+
+
 	};
 
-	if(typePost == 2){
-
-	    Micropost.active({page: this.nextPageNumber}, function(data){
-	      items = data.microposts;
-	      for (var i = 0; i < items.length; i++) {
-	        this.items.push(items[i]);
-	      }
-
-	      this.nextPageNumber += 1;
-	      this.busy = false;
-	    }.bind(this));
-	};
-
-
-	if(typePost == 3){
-
-	    Micropost.reproved({page: this.nextPageNumber}, function(data){
-	      items = data.microposts;
-	      for (var i = 0; i < items.length; i++) {
-	        this.items.push(items[i]);
-	      }
-
-	      this.nextPageNumber += 1;
-	      this.busy = false;
-	    }.bind(this));
-	};
-
-	if(typePost == 4){
-	    Micropost.pending({page: this.nextPageNumber}, function(data){
-	      var items = data.microposts;
-
-	     
-	      for (var i = 0; i < items.length; i++) {
-	        this.items.push(items[i]);
-	      }
-
-	      this.nextPageNumber += 1;
-	      this.busy = false;
-	    }.bind(this));
-	};
-
-
-	};
-
-  return ZueiraAPI;
+	return ZueiraAPI;
 });
 
 
@@ -90,7 +90,7 @@ App.controller('ApprovalsController', function($scope, Micropost,Delete_Micropos
 
 	$scope.api = new ZueiraAPI();
 	$scope.typePost = 4;
-	 $scope.letterLimit = 85;
+	$scope.letterLimit = 85;
 
 	$scope.open = function (post) {
 		$scope.post = post;
@@ -106,9 +106,9 @@ App.controller('ApprovalsController', function($scope, Micropost,Delete_Micropos
 
 
 	$scope.init = function(){
-   		MicropostParticipant.query(function(data){
-      		$scope.clubs = data.clubs;
-   		});
+		MicropostParticipant.query(function(data){
+			$scope.clubs = data.clubs;
+		});
 
 		setTimeout(function() {
 			$('.ui.modal').modal({ blurring: true }).modal();
@@ -121,19 +121,17 @@ App.controller('ApprovalsController', function($scope, Micropost,Delete_Micropos
 		$scope.post.status = 2;
 
 		$scope.micropostJson = {
-    		"micropost" :$scope.post
-    	};
+			"micropost" :$scope.post
+		};
 
-    	 var _timezueiro = $('.ui.fluid.multiple.search')
-  				.search('behavior show results');
+		var _timezueiro = $('.ui.multiple.dropdown').dropdown();
 
 
-			$log.info(_timezueiro)
+		$log.info(_timezueiro)
 
 
 		Micropost.update({ id:$scope.post.id }, $scope.micropostJson);
 
-		$('.ui.modal').modal('hide');
 		$scope.refreshTypePost($scope.typePost);
 	};
 
@@ -141,8 +139,8 @@ App.controller('ApprovalsController', function($scope, Micropost,Delete_Micropos
 		$scope.post.status = 3;
 
 		$scope.micropostJson = {
-    		"micropost" :$scope.post
-    	};
+			"micropost" :$scope.post
+		};
 
 		Micropost.update({ id:$scope.post.id }, $scope.micropostJson);
 
@@ -157,44 +155,44 @@ App.controller('ApprovalsController', function($scope, Micropost,Delete_Micropos
 		$scope.post.status = 1;
 
 		$scope.micropostJson = {
-    		"micropost" :$scope.post
-    	};
-    	
+			"micropost" :$scope.post
+		};
+		
 		Micropost.update({ id: $scope.post.id}, $scope.micropostJson);
 
 		$('.ui.modal').modal('hide');
 
 		$scope.refreshTypePost($scope.typePost)
-	
+		
 	};
 
 	$scope.deletePost = function(typePost){
 
 		$scope.micropostJson = {
-    		"micropost" :$scope.post
-    	};
-    	
+			"micropost" :$scope.post
+		};
+		
 		Micropost.delete({ id: $scope.post.id}, $scope.micropostJson);
 
 		$('.ui.modal').modal('hide');
 
 		$scope.refreshTypePost(typePost)
-	
+		
 	};
 
 	$scope.deleteSources = function(post,source,typePost){
 		$scope.post = post;
 
 		$scope.micropostJson = {
-    		"micropost" :$scope.post
-    	};
-    	
+			"micropost" :$scope.post
+		};
+		
 		Delete_Micropost.delete({ micropost_id: $scope.post.id,id_medium :source }, $scope.micropostJson);
 
 		$('.ui.modal').modal('hide');
 
 		$scope.refreshTypePost($scope.typePost)
-	
+		
 	};
 
 
