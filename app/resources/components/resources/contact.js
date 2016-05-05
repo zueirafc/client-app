ee46b1ef05a544366b7a092a9c90822164761ac3
Bruno Casali <brunoocasali@gmail.com>
2016-05-05 02:18:13 -0300

    updated contact views and controllers

--- app/resources/components/resources/contact.js
+++ app/resources/components/resources/contact.js
@@ -1,7 +1,7 @@
 App.factory('ContactFactory', function($resource, Api) {
 
   var resource = $resource(Api + '/contacts/:id.json', {}, {
-    update: { method:'PUT' },
+    update: { method: 'PUT' },
     query: { method: 'GET', isArray: false }
   });
 
