851ca6ad1ddf7ad1e2e3174579c8c897af3545c3
Bruno Casali <brunoocasali@gmail.com>
2016-05-14 14:41:43 -0300

    added giffer, remove comments in home page, started working on load post

--- app/resources/components/controllers/HomeController.js
+++ app/resources/components/controllers/HomeController.js
@@ -1,3 +1,15 @@
 App.controller('HomeController', function($scope) {
+  $scope.load = function(){
+    $('.ui.embed').embed();
+    $('.profile img').dimmer({ on: 'hover' });
+    $('.items .image img').visibility({
+      type       : 'image',
+      transition : 'fade in',
+      duration   : 1000
+    });
 
+    window.onload = function() { Gifffer(); }
+  };
+
+  $scope.load();
 });
